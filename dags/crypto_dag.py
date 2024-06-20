4dc450bd59ce167854573f09a946bb5114f77a0f
Lampros Lountzis <lamproslntz@outlook.com>
2024-06-20 15:45:17 +0300

    feat: DAG configs and extract task

--- dags/crypto_dag.py
+++ dags/crypto_dag.py
@@ -1,6 +1,11 @@
+import requests
 from airflow.decorators import dag, task
 from pendulum import datetime
-from include.global_variables import airflow_config_variables as airflow_config
+
+from include.global_variables import airflow_config, polygon_config
+
+airflow_logger = airflow_config.task_log
+
 
 @dag(
     dag_id="crypto_etl_pipe",
@@ -12,7 +17,43 @@ from include.global_variables import airflow_config_variables as airflow_config
     tags=["start", "setup"],
 )
 def crypto_pipeline():
-    print("Hello World!")
+    """ TODO: complete """
+
+    @task
+    def extract_task():
+        """ TODO: complete """
+        api_key = polygon_config.default_args["api_key"]
+
+        base_url = polygon_config.default_args["base_url"]
+
+        crypto_ticker = polygon_config.default_args["cryptoTicker"]
+        multiplier = polygon_config.default_args["multiplier"]
+        date_from = polygon_config.default_args["from"]
+        date_to = polygon_config.default_args["to"]
+        adjusted = polygon_config.default_args["adjusted"]
+        sort = polygon_config.default_args["sort"]
+
+        data = dict()
+        try:
+            response = requests.get(
+                f"{base_url}{crypto_ticker}/range/{multiplier}/day/{date_from}/{date_to}"
+                f"?adjusted={adjusted}&sort={sort}&apiKey={api_key}"
+            )
+            response.raise_for_status()
+
+            data = response.json()
+        except requests.exceptions.HTTPError:
+            airflow_logger.error(response.json()["error"])
+
+        return data
+
+    @task
+    def transform_task():
+        """ TODO: complete """
+        # response.json()["results"]
+        pass
+
+    extract_task() >> transform_task()
 
 
-dag = crypto_pipeline()
+crypto_pipeline()
--- include/global_variables/airflow_config.py
+++ include/global_variables/airflow_config.py
@@ -7,7 +7,9 @@ task_log = logging.getLogger("airflow.task")
 
 
 # DAG default arguments
+# TODO: add alerts
 default_args = {
     "retries": 1,
     "retry_delay": duration(minutes=1),
+    "depends_on_past": False,
 }
