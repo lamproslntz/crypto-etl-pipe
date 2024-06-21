9182a08e51543c61b8348d9af74954c2ecd9146d
Lampros Lountzis <lamproslntz@outlook.com>
2024-06-21 13:28:30 +0300

    feat: added transform task

--- dags/crypto_dag.py
+++ dags/crypto_dag.py
@@ -1,6 +1,6 @@
+import pendulum
 import requests
 from airflow.decorators import dag, task
-from pendulum import datetime
 
 from include.global_variables import airflow_config, polygon_config
 
@@ -10,34 +10,30 @@ airflow_logger = airflow_config.task_log
 @dag(
     dag_id="crypto_etl_pipe",
     default_args=airflow_config.default_args,
-    start_date=datetime(2020, 1, 1),
+    start_date=pendulum.datetime(2020, 1, 1),
     schedule="@daily",
     catchup=False,
     description="ETL Pipeline for Crypto Data from Polygon",
     tags=["start", "setup"],
 )
 def crypto_pipeline():
-    """ TODO: complete """
+    """TODO: complete"""
 
     @task
     def extract_task():
-        """ TODO: complete """
+        """TODO: complete"""
+        # load Polygon API configurations
         api_key = polygon_config.default_args["api_key"]
-
         base_url = polygon_config.default_args["base_url"]
-
-        crypto_ticker = polygon_config.default_args["cryptoTicker"]
-        multiplier = polygon_config.default_args["multiplier"]
-        date_from = polygon_config.default_args["from"]
-        date_to = polygon_config.default_args["to"]
-        adjusted = polygon_config.default_args["adjusted"]
-        sort = polygon_config.default_args["sort"]
+        symbol_from = polygon_config.default_args["symbol_from"]
+        symbol_to = polygon_config.default_args["symbol_to"]
+        date_capture = polygon_config.default_args["date_capture"]
+        price_adjusted = polygon_config.default_args["price_adjusted"]
 
         data = dict()
         try:
             response = requests.get(
-                f"{base_url}{crypto_ticker}/range/{multiplier}/day/{date_from}/{date_to}"
-                f"?adjusted={adjusted}&sort={sort}&apiKey={api_key}"
+                f"{base_url}/{symbol_from}/{symbol_to}/{date_capture}?adjusted={price_adjusted}&apiKey={api_key}"
             )
             response.raise_for_status()
 
@@ -48,12 +44,27 @@ def crypto_pipeline():
         return data
 
     @task
-    def transform_task():
-        """ TODO: complete """
-        # response.json()["results"]
-        pass
+    def transform_task(data):
+        """TODO: complete"""
+        symbol, currency = data["symbol"].split("-")
 
-    extract_task() >> transform_task()
+        date_capture = pendulum.parse(data["day"])
+        date_capture = date_capture.to_date_string()
+
+        # default values
+        crypto_info = {
+            "symbol": symbol,
+            "currency": currency,
+            "day": date_capture,
+            "open_price": data["open"],
+            "close_price": data["close"],
+        }
+
+        return crypto_info
+
+    transform_task(extract_task())
+    # transformed_data = transform_task(extract_task())
+    # extract_task() >> transform_task()
 
 
 crypto_pipeline()
--- include/global_variables/airflow_config.py
+++ include/global_variables/airflow_config.py
@@ -1,13 +1,12 @@
 import logging
-from pendulum import duration
 
+from pendulum import duration
 
 # get Airflow task logger
 task_log = logging.getLogger("airflow.task")
 
 
 # DAG default arguments
-# TODO: add alerts
 default_args = {
     "retries": 1,
     "retry_delay": duration(minutes=1),
