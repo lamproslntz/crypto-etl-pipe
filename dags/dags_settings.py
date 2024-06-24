f938d4b85e4dae97869f9f1a4b017b58daa3be7b
Lampros Lountzis <lamproslntz@outlook.com>
2024-06-24 16:51:02 +0300

    chore: clean-up

--- dags/dags_settings.py
+++ dags/dags_settings.py
@@ -1,7 +1,7 @@
 dags_settings = {
     "crypto": {
         "cryptos": ["BTC", "ETH", "DOGE", "MATIC"],
-        "start_date": "2024-06-01",
+        "start_date": "2024-06-01",  # YYYY-MM-DD
         "retries": 5,
     },
 }
